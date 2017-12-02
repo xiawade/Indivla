f62f006f68e397a8d7896c806a859bee9c455073
Fanyao Xia <fanyao.xia@bellmai.com>
2017-12-02 17:52:39 -0500

    slider image show version 1 of home page

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -1,4 +1,148 @@
 @extends('welcome')
+
 @section('block')
-	<div class="childDiv">I am a Child Div</div>
+	<div class="childDiv carousel slide" data-ride="carousel" data-interval="2000">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3000">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+	--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+	--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3040">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+	--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+	--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="9000">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="10000">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="11300">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="4050">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="2060">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="3600">
+			
+	</div><!--
+		--><div class="childDiv carousel slide" data-ride="carousel" data-interval="5500">
+			<div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			    </div>
+  			</div>
+	</div><!--
+		--><div class="childDiv">
+			
+	</div>
 @endsection
