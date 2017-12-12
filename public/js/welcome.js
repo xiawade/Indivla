2a580d3c2f3b4c11a9342ff5812c51b6fdf83a87
Fanyao Xia <fanyaoxia@FanyaodeMacBook-Pro-2.local>
2017-12-12 14:58:08 -0500

    Menu bar effect done

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,9 +1,18 @@
 /* Set the width of the side navigation to 250px */
 $(function(){
 	$('#menuIcon').click(function(){
-		$('#mySidenav').css('width','250px');
-	})
-	$('.closebtn').click(function(){
-		$('#mySidenav').css('width','0');
+		if($('#menuBar').css('visibility')=='hidden'){
+				$('#menuBar').css('visibility','visible');
+				$('#menuBar').css('opacity','1');
+				$('#menuIcon img').css('transform','rotate(90deg)');
+				$('#menuIcon img').css('transition','all 0.5s ease');
+				$('#menuBar').css('transition','all 0.5s ease');
+		}else{
+				$('#menuBar').css('visibility','hidden');
+				$('#menuBar').css('opacity','0');
+				$('#menuBar').css('transition','all 0.5s ease');
+				$('#menuIcon img').css('transform','rotate(180deg)');
+				$('#menuIcon img').css('transition','all 0.5s ease');
+		}
 	})
 });
--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -4,17 +4,34 @@
 
 @parent
 
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn">&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
+<div id="menuBar">
+	<div class="menuLineOne">
+		<div class="oneItems">
+  			<a href="#">ABOUT ME</a>
+		</div>
+		<div class="oneItems">
+			<a href="#">JOURNEY</a>
+		</div>
+		<div class="oneItems">
+			<a href="#">POST</a>
+		</div>
+	</div>
+	<div class="menuLineTwo">
+		<div class="twoItems">
+  			<a href="#">CONTACT</a>
+		</div>
+		<div class="twoItems">
+  			<a href="#">SKILLS</a>
+		</div>
+	</div>
+	<div class="menuLineThree">
+		<div class="threeItems">
+  			<a href="#">FOCUS</a>
+		</div>
+	</div>
 </div>
 
 <!-- Use any element to open the sidenav -->
-<span id="menuIcon"><img src="{{asset('img/menuIcon.png')}}"/></span>
+<span id="menuIcon"><img src="{{asset('img/oneLine.jpg')}}"/></span>
 
-<div id="main">
-</div>
 @endsection
