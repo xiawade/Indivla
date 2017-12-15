c087cef875c1abbe0a60523b808760e891f92eda
Fanyao Xia <fanyao.xia@bellmai.com>
2017-12-15 18:46:00 -0500

    changed to a dark theme with big name title

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,18 +1,9 @@
 /* Set the width of the side navigation to 250px */
 $(function(){
-	$('#menuIcon').click(function(){
-		if($('#menuBar').css('visibility')=='hidden'){
-				$('#menuBar').css('visibility','visible');
-				$('#menuBar').css('opacity','1');
-				$('#menuIcon img').css('transform','rotate(90deg)');
-				$('#menuIcon img').css('transition','all 0.5s ease');
-				$('#menuBar').css('transition','all 0.5s ease');
-		}else{
-				$('#menuBar').css('visibility','hidden');
-				$('#menuBar').css('opacity','0');
-				$('#menuBar').css('transition','all 0.5s ease');
-				$('#menuIcon img').css('transform','rotate(180deg)');
-				$('#menuIcon img').css('transition','all 0.5s ease');
-		}
-	})
+	showText();
 });
+
+function showText(){
+	$('#myName p').css('display','');
+	$('#indicator').css('display','');
+}
--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -4,34 +4,9 @@
 
 @parent
 
-<div id="menuBar">
-	<div class="menuLineOne">
-		<div class="oneItems">
-  			<a href="#">ABOUT ME</a>
-		</div>
-		<div class="oneItems">
-			<a href="#">JOURNEY</a>
-		</div>
-		<div class="oneItems">
-			<a href="#">POST</a>
-		</div>
-	</div>
-	<div class="menuLineTwo">
-		<div class="twoItems">
-  			<a href="#">CONTACT</a>
-		</div>
-		<div class="twoItems">
-  			<a href="#">SKILLS</a>
-		</div>
-	</div>
-	<div class="menuLineThree">
-		<div class="threeItems">
-  			<a href="#">FOCUS</a>
-		</div>
-	</div>
+<div id="middleBlock">
+	<div id="myName"><p>FANYAO XIA</p></div>
+	<div id="indicator"><p>SCROLL DOWN TO THINK</p></div>
 </div>
-
-<!-- Use any element to open the sidenav -->
-<span id="menuIcon"><img src="{{asset('img/oneLine.jpg')}}"/></span>
 
 @endsection
